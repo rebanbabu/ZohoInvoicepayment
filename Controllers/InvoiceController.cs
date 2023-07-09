5fbeada5a929a33cd169d460d0a564bf43bb1e86
rebanbabu <53808103+rebanbabu@users.noreply.github.com>
2023-07-09 15:25:52 +0530

    Add files via upload

---
+++ Controllers/InvoiceController.cs
@@ -0,0 +1,84 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Zoho.Models;
+
+namespace Zoho.Controllers;
+
+public class InvoicesController : Controller
+{
+    private readonly InvoiceRepository _invoiceRepository;
+
+    public InvoicesController(InvoiceRepository invoiceRepository)
+    {
+        _invoiceRepository = invoiceRepository;
+    }
+
+    // GET: /Invoices
+    public ActionResult Index()
+    {
+        List<Invoice> invoices = _invoiceRepository.GetAll();
+        return View(invoices);
+    }
+
+    // GET: /Invoices/Create
+    public ActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: /Invoices/Create
+    [HttpPost]
+    public ActionResult Create(Invoice invoice)
+    {
+        if (ModelState.IsValid)
+        {
+            _invoiceRepository.Create(invoice);
+            return RedirectToAction("Index");
+        }
+        return View(invoice);
+    }
+
+    // GET: /Invoices/Edit/5
+    public ActionResult Edit(int id)
+    {
+        Invoice invoice = _invoiceRepository.GetById(id);
+        if (invoice == null)
+        {
+            return HttpNotFound();
+        }
+        return View(invoice);
+    }
+
+    // POST: /Invoices/Edit/5
+    [HttpPost]
+    public ActionResult Edit(Invoice invoice)
+    {
+        if (ModelState.IsValid)
+        {
+            _invoiceRepository.Update(invoice);
+            return RedirectToAction("Index");
+        }
+        return View(invoice);
+    }
+
+    // GET: /Invoices/Delete/5
+    public ActionResult Delete(int id)
+    {
+        Invoice invoice = _invoiceRepository.GetById(id);
+        if (invoice == null)
+        {
+            return HttpNotFound();
+        }
+        return View(invoice);
+    }
+
+    // POST: /Invoices/Delete/5
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+        Invoice invoice = _invoiceRepository.GetById(id);
+        _invoiceRepository.Delete(invoice);
+        return RedirectToAction("Index");
+    }
+}
+
---
+++ Models/InvoicePaymentModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Zoho.Models;
+
+public class Invoice
+{
+    public int InvoiceId { get; set; }
+    public string InvoiceNumber { get; set; }
+    public string CustomerName { get; set; }
+    public decimal Amount { get; set; }
+    
+}
+
+public class Payment
+{
+    public int PaymentId { get; set; }
+    public int InvoiceId { get; set; }
+    public decimal Amount { get; set; }
+    public DateTime PaymentDate { get; set; }
+   
+}
+
